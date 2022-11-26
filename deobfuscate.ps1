20ab912a40f4ad28d1893af5553e198398a65d19
Jack Baron <jackmbaron@gmail.com>
2022-11-26 22:22:36 +0000

    add powershell script to run deobfuscation

---
+++ deobfuscate.ps1
@@ -0,0 +1,43 @@
+param (
+  [string]$Version,
+  [bool]$DryRun
+)
+
+function Test-Tool {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$Tool
+  )
+
+  if (Get-Command -ErrorAction SilentlyContinue $Tool) {} else {
+    Write-Output "$Tool not detected!"
+    Write-Output "Please add $Tool to your PATH"
+
+    Exit 1
+  }
+}
+
+# Ensure CLI tools are installed
+Test-Tool "git"
+Test-Tool "msbuild"
+Test-Tool "dotnet"
+
+# Ensure submodule is setup
+git submodule update --init
+
+# Resolve paths
+$project = Resolve-Path -Path ./Scoresaber-Deobfuscate/Deobfuscator.Bulk
+$versions = Resolve-Path -Path ./versions.tsv
+
+# Build command
+$command = "dotnet run --project '$project' -- --versions '$versions' --password velvet99"
+if ($version) {
+  $command = "$command -V '$version'"
+}
+
+if ($DryRun) {
+  $command = "$command --dry-run"
+}
+
+# Run
+Invoke-Expression $command
